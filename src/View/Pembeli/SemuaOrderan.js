fcedbf9c5b46eea5ba747c80e9f021f35c5996ed
Azka Andya <azkaandyaaa@gmail.com>
2020-12-18 17:16:13 +0700

    fixing bug

--- src/View/Pembeli/SemuaOrderan.js
+++ src/View/Pembeli/SemuaOrderan.js
@@ -34,7 +34,7 @@ const styles = theme=>({
   table: {
     minWidth: 700,
   },
-});
+}); 
 
 class SemuaOrderan extends React.Component{
     constructor(){
@@ -52,7 +52,7 @@ class SemuaOrderan extends React.Component{
         var header = new Headers();
         header.append("Content-Type","application/json");
             console.log(decoded.id_user)
-        fetch(`http://140.238.205.80/pesanan/pembeli/ ${decoded.id_user}`,{
+        fetch(`http://140.238.205.80/pesanan/pembeli/${decoded.id_user}`,{
             method:"GET",
             header:header,
         })
